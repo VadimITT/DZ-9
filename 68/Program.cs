fcfd3d8b6b4b9347c4c0f0e93ed86d5cf081041b
Vadim <opizah70@gmail.com>
2022-12-26 00:28:29 +0500

    -

---
+++ 68/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число : ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число : ");
+int b = int.Parse(Console.ReadLine());
+
+AkkermanFunction(a, b);
+void AkkermanFunction(int a, int b)
+{
+    Console.Write(Akkerman(a, b));
+}
+int Akkerman(int a, int b)
+{
+    if (a == 0)
+    {
+        return b + 1;
+    }
+    else if (a == 0 && b > 0)
+    {
+        return Akkerman(a - 1, 1);
+    }
+    else
+    {
+        return Akkerman(a, b);
+    }
+}
---
+++ task 66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите начальное число :");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число :");
+int number2 = int.Parse(Console.ReadLine());
+void NumberSum(int number1, int number2, int sum)
+{
+    if (number1 > number2)
+    {
+        Console.WriteLine($"Сумма натуральных элементов {sum}");
+        return;
+    }
+    sum = sum + (number1++);
+    NumberSum(number1, number2, sum);
+}
+
+NumberSum(number1, number2, 0);
